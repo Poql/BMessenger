1f20a2792f34c0400d7db8461a5ae822ff09f86b
GaetanZanella <gaetan.zanella@gmail.com>
2016-05-22 11:32:48 +0200

    Add parse of messages in parser

---
+++ parser.js
@@ -0,0 +1,76 @@
+'use strict'
+
+const MessageType = {
+	classes: "classes",
+	add_class: "add_class",
+	remove_class: "remove_class",
+	remove: "remove",
+	publish: "publish",
+	unparsed: "unparsed"
+}
+
+const CMD_CLASSES = "classes"
+const CMD_REMOVE_CLASS = "remove "
+const CMD_ADD_CLASS = "add "
+
+
+class Parser {
+	parseMessage(msg) {
+		var result = {
+			messageType: undefined,
+			payload: undefined
+		}
+
+		let gross = msg.trim()
+		let message = gross.toLowerCase()
+
+		if(message == CMD_CLASSES){
+			result.messageType = MessageType.classes
+		}
+		else if(message.substring(0, CMD_REMOVE_CLASS.length) == CMD_REMOVE_CLASS) {
+			result.messageType = MessageType.remove_class
+			result.payload = gross.substring(CMD_REMOVE_CLASS.length, gross.length)
+		}
+		else if(message == "remove") {
+			result.messageType = MessageType.remove
+		}
+		else if(message.substring(0, 1) == "@") {
+			var words = gross.split(" ")
+			var classes = []
+			var msg = ""
+
+			words.forEach((word) => {
+				if(word.substring(0,1) == "@") {
+					let classe = word.substring(1,word.length).toLowerCase()
+					if(classes.indexOf(classe) == -1) {
+						classes.push(classe)	
+					}
+				}
+				else {
+					msg += (word + " ")
+				}
+			})
+			result.messageType = MessageType.publish
+
+			result.payload = {
+				classes: classes,
+				message: msg.trim()
+			}
+
+		}
+		else if(message.substring(0, CMD_ADD_CLASS.length) == CMD_ADD_CLASS)  {
+			result.messageType = MessageType.add_class
+			result.payload = gross.substring(CMD_ADD_CLASS.length, gross.length)
+		}
+		else {
+			result.messageType = MessageType.unparsed
+			result.payload = gross
+		}
+		return result
+	}
+}
+
+module.exports = {
+	Parser: Parser,
+	MessageType: MessageType
+}
