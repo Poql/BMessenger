4b1aa7bb7216ff8f654e196df670a4ec6d895efe
Gaétan Zanella <gaetanzanella@MacBook-Pop.local>
2016-05-29 10:34:32 +0200

    Add MongoDB to dataSource

--- dataSource.js
+++ dataSource.js
@@ -1,5 +1,14 @@
 'use strict'
 const Model = new require("./Model")
+const DBModel = new require("./dbModel")
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/ZanellaDB')
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+	console.log("DB open")
+});
 
 class DataSource {
 
